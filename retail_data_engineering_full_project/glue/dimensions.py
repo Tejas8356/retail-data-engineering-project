3903666be093a99c68720749d4f3c5771976c0a3
Tejas8356 <68561642+Tejas8356@users.noreply.github.com>
2026-04-19 02:27:47 +0530

    Update dimensions.py

--- retail_data_engineering_full_project/glue/dimensions.py
+++ retail_data_engineering_full_project/glue/dimensions.py
@@ -1,28 +1,77 @@
-
+import sys
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+# Initialize
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
 job = Job(glueContext)
-job.init("dimensions", {})
+job.init("retail-dimensions-transform", {})
 
-# customers
-spark.read.parquet("s3://retail-dwh-processed/customers/")\
-    .dropDuplicates()\
-    .write.mode("overwrite").parquet("s3://retail-dwh-gold/dim_customers/")
+print("Starting Dimension Tables Creation...")
 
-# products
-spark.read.parquet("s3://retail-dwh-processed/products/")\
-    .dropDuplicates()\
-    .write.mode("overwrite").parquet("s3://retail-dwh-gold/dim_products/")
+# =========================
+# DIM CUSTOMERS
+# =========================
+print("Processing dim_customers...")
+customers_df = spark.read.parquet("s3://retail-dwh-processed/customers/")
 
-# stores
-spark.read.parquet("s3://retail-dwh-processed/stores/")\
-    .dropDuplicates()\
-    .write.mode("overwrite").parquet("s3://retail-dwh-gold/dim_stores/")
+dim_customers = customers_df.select(
+    "customer_id",
+    "first_name",
+    "last_name",
+    "email",
+    "city",
+    "country"
+).dropDuplicates()
+
+dim_customers.write.mode("overwrite").parquet(
+    "s3://retail-dwh-gold/dim_customers/"
+)
+
+print("dim_customers created!")
+
+# =========================
+# DIM PRODUCTS
+# =========================
+print("Processing dim_products...")
+products_df = spark.read.parquet("s3://retail-dwh-processed/products/")
+
+dim_products = products_df.select(
+    "product_id",
+    "product_name",
+    "category",
+    "cost_price"
+).dropDuplicates()
+
+dim_products.write.mode("overwrite").parquet(
+    "s3://retail-dwh-gold/dim_products/"
+)
+
+print("dim_products created!")
+
+# =========================
+# DIM STORES
+# =========================
+print("Processing dim_stores...")
+stores_df = spark.read.parquet("s3://retail-dwh-processed/stores/")
+
+dim_stores = stores_df.select(
+    "store_id",
+    "store_name",
+    "city",
+    "country"
+).dropDuplicates()
+
+dim_stores.write.mode("overwrite").parquet(
+    "s3://retail-dwh-gold/dim_stores/"
+)
+
+print("dim_stores created!")
+
+print("All dimension tables created successfully!")
 
 job.commit()
